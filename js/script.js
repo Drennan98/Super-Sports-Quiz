07cc0e2883b95553a045213274c7b67919a55fda
Drennan98 <>
2024-01-04 17:58:32 +0000

    Add quiz questions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
+const questions = [
+    {
+        question: "Tiger Woods won his first major in over a decade at which tournament in 2019?",
+        options: ["The US Open, The Masters, The Open, The US PGA Championship"],
+        answer: "The Masters"
+    },
+    {
+        question: "Who won the World Cup in 2022?",
+        options: ["France, Poland, Spain, Argentina"],
+        answer: "Argentina"
+    },
+    {
+        question: "How many doubles are there on a dart board?",
+        options: ["12", "8", "20", "16"],
+        answer: "20"
+    },
+    {
+        question: "What is the maximum break you can get in snooker?",
+        options: ["145", "147", "143", "141"],
+        answer: "147"
+    },
+    {
+        question: "How many Formula 1 World Championships does Lewis Hamilton have?",
+        options: ["3", "8", "7", "5"],
+        answer: "7"
+    }
+];
+
+
 function showQuestion {
 
 }
 
-function testReponse {
-    
+function checkResponse {
+
 }
 
 function nextQuestion {
